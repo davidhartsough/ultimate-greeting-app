eeaedb4333f193fc717b92791f62c35f7e54b97e
David Hartsough <hartsoughdavid@gmail.com>
2020-01-07 04:00:14 -0700

    Updates

---
+++ precache-manifest.3136d6e243137330bc1917c9571a5891.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5eaa5e856669dd7e108752a90c6a12f5",
+    "url": "/ultimate-greeting-app/index.html"
+  },
+  {
+    "revision": "15f1f0a1de397fed1970",
+    "url": "/ultimate-greeting-app/static/css/main.924720be.chunk.css"
+  },
+  {
+    "revision": "e3e9910e5f32dd6854c3",
+    "url": "/ultimate-greeting-app/static/js/2.2cc33140.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/ultimate-greeting-app/static/js/2.2cc33140.chunk.js.LICENSE"
+  },
+  {
+    "revision": "15f1f0a1de397fed1970",
+    "url": "/ultimate-greeting-app/static/js/main.b16ac182.chunk.js"
+  },
+  {
+    "revision": "43670311ffa86fca2462",
+    "url": "/ultimate-greeting-app/static/js/runtime-main.e36279a1.js"
+  }
+]);
